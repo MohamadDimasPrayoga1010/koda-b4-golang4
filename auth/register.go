325c51c673579fd85997a625876b7490f8ca6233
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-10-22 22:24:44 +0700

    feat: improve validation

--- auth/register.go
+++ auth/register.go
@@ -22,50 +22,59 @@ func (a *Auth) Register() {
 	}()
 
 	reader := bufio.NewReader(os.Stdin)
-	var firstName, lastName, email, password string
 
-	for {
-		var confirmPassword string
-		fmt.Println("=====Register=====")
-		fmt.Print("Whats is your first name: ")
-		fmt.Scan(&firstName)
-		fmt.Print("Whats is your last name: ")
-		fmt.Scan(&lastName)
-		fmt.Print("Whats is your Email: ")
-		fmt.Scan(&email)
-		fmt.Print("Enter a strong password: ")
-		fmt.Scan(&password)
-		fmt.Print("Confirm your Password: ")
-		fmt.Scan(&confirmPassword)
+	for { 
+		var firstName, lastName, email, password string
 
-		if password != confirmPassword {
-			fmt.Println("Whrong confirm password, press enter to back")
-			reader.ReadString('\n')
+		for { 
+			var confirmPassword string
+			fmt.Println("=====Register=====")
+			fmt.Print("Whats is your first name: ")
+			fmt.Scan(&firstName)
+			fmt.Print("Whats is your last name: ")
+			fmt.Scan(&lastName)
+			fmt.Print("Whats is your Email: ")
+			fmt.Scan(&email)
+			fmt.Print("Enter a strong password: ")
+			fmt.Scan(&password)
+			fmt.Print("Confirm your Password: ")
+			fmt.Scan(&confirmPassword)
+
+			if password != confirmPassword {
+				fmt.Println("Wrong confirm password, press Enter to try again...")
+				reader.ReadString('\n') 
+				continue
+			}
+			break
+		}
+
+		fmt.Println("\nIs it correct?")
+		fmt.Println("FirstName:", firstName)
+		fmt.Println("LastName :", lastName)
+		fmt.Println("Email    :", email)
+
+		var confirm string
+		fmt.Print("Continue? (y/n): ")
+		fmt.Scan(&confirm)
+		confirm = strings.ToLower(confirm)
+
+		if confirm == "y" {
+			a.Users = append(a.Users, &User{
+				Firstname: firstName,
+				LastName:  lastName,
+				Email:     email,
+				Password:  a.EncryptPassword(password),
+			})
+
+			fmt.Println("Register success, press enter to back..")
+			reader.ReadString('\n') 
+			break 
+		} else if confirm == "n" {
+			fmt.Println("Registration canceled, please refill the data...\n")
+			continue 
+		} else {
+			fmt.Println("Invalid input, please try again...\n")
 			continue
 		}
-		break
 	}
-
-	fmt.Println("\nIs it correct?")
-	fmt.Println("FirstName:", firstName)
-	fmt.Println("LastName :", lastName)
-	fmt.Println("Email    :", email)
-
-	var confirm string
-	fmt.Print("Continue? (y/n): ")
-	fmt.Scan(&confirm)
-	confirm = strings.ToLower(confirm)
-	if confirm != "y" {
-		fmt.Println("Register dibatalkan!")
-		return
-	}
-
-	a.Users = append(a.Users, &User{
-		Firstname: firstName,
-		LastName:  lastName,
-		Email:     email,
-		Password:  a.EncryptPassword(password),
-	})
-
-	fmt.Println("Register berhasil!")
-} 
+}
