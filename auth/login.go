d17ecee4759abaf2aca661288ef959b1b76d2a8e
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-10-22 22:23:58 +0700

    feat: fix the login feature when the user is successful, they immediately enter the home page

--- auth/login.go
+++ auth/login.go
@@ -1,7 +1,9 @@
 package auth
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func (a *Auth) Login() {
@@ -10,22 +12,34 @@ func (a *Auth) Login() {
 			fmt.Println("Gagal Login")
 		}
 	}()
-	// reader := bufio.NewReader(os.Stdin)
 
-	var email, password string
-	fmt.Println("\n=====Login=====")
-	fmt.Print("Enter your email : ")
-	fmt.Scan(&email)
-	fmt.Print("Enter your password")
-	fmt.Scan(&password)
+	reader := bufio.NewReader(os.Stdin)
 
-	for _, u := range a.Users {
-		if u.Email == email && u.Password == a.EncryptPassword(password) {
-			fmt.Println("Login berhasil! Welcome", u.Firstname)
-			return
+	for {
+		var email, password string
+		fmt.Println("\n===== Login =====")
+		fmt.Print("Enter your email: ")
+		fmt.Scan(&email)
+		fmt.Print("Enter your password: ")
+		fmt.Scan(&password)
+
+		found := false
+		for _, u := range a.Users {
+			if u.Email == email && u.Password == a.EncryptPassword(password) {
+				fmt.Println("Login berhasil! Welcome", u.Firstname)
+				fmt.Println("Press Enter to return to the main menu...")
+				reader.ReadString('\n') 
+				found = true
+				a.Home(u.Firstname)
+				break
+			}
+		}
+
+		if found {
+			break 
+		} else {
+			fmt.Println("Wrong Email or Password! Press Enter to try again...")
+			reader.ReadString('\n') 
 		}
 	}
-
-	fmt.Println("Email atau Password salah!")
-
 }
